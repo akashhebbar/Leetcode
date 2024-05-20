9ec92b395b1539ee1f29c7dd49fd7a726a8ed2cc
Aakash Hebbar <46225653+akashhebbar@users.noreply.github.com>
2024-05-20 08:10:59 +0530

    flatten_list

--- PythonBasics/Lists/List.py
+++ PythonBasics/Lists/List.py
@@ -22,6 +22,25 @@ mylist.extend(newList)
 mylist.insert(0, 100)
 print(mylist)
 
-sample_list = [10, 20, 10, 3, 4, 1, 40, 50]
-sample_list.sort(key=lambda x: -x)
-print("sorted", sample_list)
+sample_list = [10, 20, 10, 3, 4, 1, 40, 50, ["akash", "ram", "kumar"], {2, 4, 5, 6, 7, 8, 9}]
+
+
+# sample_list.sort(key=lambda x: -x)
+# print("sorted", sample_list)
+# from itertools import chain
+#
+# test_list = list(chain.from_iterable(sample_list))
+# print(test_list)
+
+def flatten_list(input_list):
+    for item in input_list:
+        if isinstance(item, list):
+            yield from flatten_list(item)
+        elif isinstance(item, set):
+            yield from item
+        else:
+            yield item
+
+
+ans = list(flatten_list(sample_list))
+print("ans", ans)
