ff24d60b6435d72cf6cb57bf571614a963eaa963
Aakash Hebbar <46225653+akashhebbar@users.noreply.github.com>
2024-05-23 07:20:18 +0530

    Update dict.py

--- PythonBasics/Lists/dict.py
+++ PythonBasics/Lists/dict.py
@@ -30,11 +30,50 @@ print("The original dictionary : " + str(test_dict))
 # Swapping Hierarchy in Nested Dictionaries
 # Using loop + items()
 res = dict()
-for key, val in test_dict.items():
-    for key_in, val_in in val.items():
-        if key_in not in res:
-            temp = dict()
-        else:
-            temp = res[key_in]
-        temp[key] = val_in
-        res[key_in] = temp
+# for key, val in test_dict.items():
+#     for key_in, val_in in val.items():
+#         if key_in not in res:
+#             temp = dict()
+#         else:
+#             temp = res[key_in]
+#         temp[key] = val_in
+#         res[key_in] = temp
+
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+dt = {}
+for k, v in zip(keys, values):
+    dt[k] = v
+
+print(dt)
+
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+dict1.update(dict2)
+print(dict1)
+
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+
+
+def getkey(sampleDict, target):
+    for key, values in sampleDict.items():
+        if key == target:
+            print(values)
+        elif isinstance(values, dict):
+            getkey(values, target)
+
+
+getkey(sampleDict, 'history')
