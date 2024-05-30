04dc2f30d2631ef40ff0b08aad7fc05066ed9488
Aakash Hebbar <46225653+akashhebbar@users.noreply.github.com>
2024-05-30 07:18:30 +0530

    Cache Dec

---
+++ Decorotor/MemorizeDec.py
@@ -0,0 +1,25 @@
+# dec to memorize function
+
+def memorize(func):
+    cache = {}
+
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+
+    return wrapper
+
+
+@memorize
+def getAge(age):
+    if age < 18:
+        return "Under 18 years"
+    else:
+        return "18+ years"
+
+
+ans = getAge(18)
+print(ans)
+ans1 = getAge(18)
+print(ans1)
