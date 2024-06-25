276a99358877cbfbf837cb64f59d3386530f72ee
Aakash Hebbar <46225653+akashhebbar@users.noreply.github.com>
2024-06-25 07:50:04 +0530

    insertion_sort

---
+++ PythonBasics/Searching Algorithms/insertion_sort.py
@@ -0,0 +1,15 @@
+# select item and insert into correct position
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+def insertion_sort(arr: list):
+    for i in range(1, len(arr)):
+        while i > 0 and arr[i] < arr[i - 1]:
+            swap(arr, i, i - 1)
+            i -= 1
+
+
+items = [20, 21, 4, -1, -8, 66, 22, 4, 5, 21, 684, 99, 32, 53]
+insertion_sort(items)
+print(items)
