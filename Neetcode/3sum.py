a3f9ff77a602080448fede7174d9dda5d74e5297
Aakash Hebbar <46225653+akashhebbar@users.noreply.github.com>
2024-07-24 00:06:00 +0530

    3sum

---
+++ Neetcode/3sum.py
@@ -0,0 +1,16 @@
+nums = [-1, 0, 1, 2, -1, -4]
+
+
+def get_sum(nums: list) -> list:
+    ans = []
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            for k in range(j + 1, len(nums)):
+                if i != j and i != j and j != k and nums[i] + nums[j] + nums[k] == 0:
+                    ans.append([nums[i], nums[j], nums[k]])
+
+    return ans
+
+
+result = get_sum(nums)
+print(result)
